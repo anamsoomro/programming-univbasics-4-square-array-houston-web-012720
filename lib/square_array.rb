9b454f1d85cc262769c87ea42375ba6986b4592d
anam soomro <anamsoomroed@gmail.com>
2019-12-11 02:15:04 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,10 +3,9 @@ def square_array(array)
   squared_array = []
   while array[index] do
   squared_array.push(array[index]**2)
-  index=index+1
-  #why does it not work if i put index+=
+  index += 1
   end
   p squared_array
-  #if you use puts it will return nil 
+  #if you use puts it will return nil remember
 end
 
